d584a49d22b03d4ad3867c68691e06b6a3017795
Julius Sonko <jsonko@my.edgetechacademy.edu>
2020-05-04 10:48:34 -0500

    Add project files.

---
+++ Looping/GuessMyNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Looping
+{
+    class GuessMyNumber
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int hidden;
+            int guessedNumber;
+            string playAgain;
+            string strHotCold;
+
+             do
+            {
+                hidden = rand.Next(100);
+                guessedNumber = Utils.GetNumber("Guess a number between 0 and 100: ", 100);
+
+                while (true)
+                {
+                    int diff = Math.Abs(guessedNumber) - Math.Abs(hidden);
+                    if (diff > 10)
+                    {
+                        strHotCold = "You're as cold as ice!";
+                    }
+                    else if (diff > 5)
+                    {
+                        strHotCold = "You're getting warm!";
+                    }
+                    else if (diff > 3)
+                    {
+                        strHotCold = "You're getting hot!";
+                    }
+                    else
+                    {
+                        strHotCold = "You're on Fire!!";
+                    }
+                    if (guessedNumber > hidden)
+                    {
+                        Console.WriteLine("Your guessed number is greater than my hidden number");
+                        Console.WriteLine(strHotCold);
+                    }
+                    else if (guessedNumber < hidden)
+                    {
+                        Console.WriteLine("Your guessed number is lower than my hidden number");
+                        Console.WriteLine(strHotCold);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guessed number is correct! WINNER!");
+                        playAgain = Utils.GetInput("Would you like to play again? (Y for Yes): ");
+                        break;
+                    }
+                    guessedNumber = Utils.GetNumber("Guess a number between 0 and 100: ", 100);
+                }
+            }
+            while (playAgain.ToUpper() == "Y");
+        }
+
+    }
+}
---
+++ Looping/LargestandSmallestNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Looping
+{
+    class LargestandSmallestNumbers
+    {
+        static void Main(string[] args)
+        {
+            int nLargest;
+            int nSmallest;
+            int nCurrent;
+            string strEntry;
+
+            Console.WriteLine("Enter an integer: ");
+            nLargest = nSmallest = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another integer or Q to Quit: ");
+            strEntry = Console.ReadLine();
+
+            while (strEntry != "Q")
+            {
+                nCurrent = Convert.ToInt32(strEntry);
+
+                if (nCurrent > nLargest)
+                {
+                    nLargest = nCurrent;
+                }
+                else if (nCurrent < nSmallest)
+                {
+                    nSmallest = nCurrent;
+                }
+                Console.WriteLine("Enter another integer or Q to Quit: ");
+                strEntry = Console.ReadLine();
+            }
+            Console.WriteLine("The largest value you entered was " + nLargest + ".");
+            Console.WriteLine("The smallest value you entered was " + nSmallest + ".");
+
+            Console.ReadLine();
+        }
+    }
+}
